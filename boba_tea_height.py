74f24502f170e98ad0bbcfd421d5c694ff4fe0e3
XiangFanYu <44479594+XiangFanYu@users.noreply.github.com>
2020-09-25 05:37:04 -0700

    Create boba_tea_height.py

---
+++ boba_tea_height.py
@@ -0,0 +1,22 @@
+import math
+
+print("Boba Tower Time")
+
+#height and whatever math
+print("How tall are you in feet and inches?")
+
+feet = int(input("Feet?"))
+inches = int(input("Inches?"))
+
+totalHeight = (feet * 12) + inches
+
+cups = totalHeight/7.6
+
+numCups = math.ceil(cups)
+
+oz = numCups * 17
+
+numCups = str(numCups)
+ounces = str(oz)
+print("You need to drink " + numCups + " cups of boba to reach your height")
+print("That is about " + ounces + " ounces of boba")
